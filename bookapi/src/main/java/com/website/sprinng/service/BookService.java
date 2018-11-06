1d4f54400d7add501eb254b7b469abcad30688f9
pikapika <36557956+ejs10@users.noreply.github.com>
2018-11-06 17:14:06 +0900

    강좌  add

---
+++ bookapi/src/main/java/com/website/sprinng/service/BookService.java
@@ -0,0 +1,24 @@
+package com.website.sprinng.service;
+
+import java.util.List;
+
+import com.website.sprinng.model.Book;
+
+public interface BookService {
+	
+	//Save the record
+	long save(Book book);
+	
+	//Get a single record
+	Book get(long id);
+	
+	//Get all the records
+	List<Book> list();
+	
+	//Update the record
+	void update(long id, Book book);
+	
+	//Delete a record
+	void delete(long id);
+
+}
